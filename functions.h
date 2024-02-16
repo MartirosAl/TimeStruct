a18ad159c6994f4f0f458ddd363e3921b02383d7
MartirosAl <145045481+MartirosAl@users.noreply.github.com>
2024-02-16 19:47:17 +0300

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,1 +1,19 @@
 #pragma once
+#include <stdbool.h>
+#include "structs.h"
+
+/*
+* @brief Проверка на високосный год
+* @param year Год требующий проверки
+* @return 1 Если год високосный, 0 иначе
+*/
+bool IsLeapYear(unsigned int year);
+
+/*
+* @brief Разница между двумя датами
+* @param Data_Time Дата #1
+* @param Today_Time Дата #2
+* @return Разница между датой #1 и датой #2 в секундах
+*/
+long long int DataDifference(struct TimeData Data_Time, struct TimeData Today_Time);
+
