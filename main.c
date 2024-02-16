ed50523918a8c2b383eb676b2ff925b6a0b037ba
MartirosAl <145045481+MartirosAl@users.noreply.github.com>
2024-02-16 19:40:07 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,17 +1,43 @@
 #include <stdio.h>
-#include "structs.h"
+#include "functions.h"
 
 int main()
 {
-	struct TimeData Data_Time;
+	system("chcp 1251"); //Русский язык
+	system("cls");
 
-	printf("Enter date and time in format day month year hours minuts seconds: ");
-	scanf_s("%d %d %d %d %d %d", &Data_Time.year, &Data_Time.month, &Data_Time.day, &Data_Time.hour, &Data_Time.minute, &Data_Time.second);
-	if (Data_Time.month > 12 || Data_Time.day > 31 || Data_Time.hour > 23 || Data_Time.minute > 59 || Data_Time.second > 59)
+	struct TimeData Data_Time; //Структура даты и времени
+	struct TimeData Today_Time = { 10,2,2024,13,45,00 };
+
+	printf("Enter date and time in format day.month.year hours:minuts:seconds: "); //Ввод данных 
+	scanf_s("%d.%d.%d %d:%d:%d", &Data_Time.day, &Data_Time.month, &Data_Time.year, &Data_Time.hour, &Data_Time.minute, &Data_Time.second);
+
+	//Проверки на правильность данных 
+	if (Data_Time.month > 12 || (Data_Time.month % 2 == 0 ? Data_Time.day > 31 : Data_Time.day > 30) || 
+										Data_Time.hour > 23 || Data_Time.minute > 59 ||Data_Time.second > 59)
 	{
-		printf("ERROR DATA");
+		printf("ERROR DATA\n");
 		return 0;
 	}
-	printf("%d", Data_Time.day);
+
+	//Проверка на високосный год и 29 февраля
+	if (!IsLeapYear(Data_Time.year))
+		if (Data_Time.month == 2 && Data_Time.day > 28)
+		{
+			printf("ERROR DATA\n");
+			return 0;
+		}
+		else
+			printf("Это не високосный год\n");
+	else
+		printf("Это високосный год\n");
+
+	printf("%d.%d.%d %d:%d:%d\n", Data_Time.day, Data_Time.month, Data_Time.year, Data_Time.hour, Data_Time.minute, Data_Time.second);
+	printf("%d.%d.%d %d:%d:%d\n", Today_Time.day, Today_Time.month, Today_Time.year, Today_Time.hour, Today_Time.minute, Today_Time.second);
+
+	long long int difference = DataDifference(Data_Time, Today_Time);
+	printf("Различается в %lld секунд \n в %lld минут \n в %lld часов \n в %lld дней \n в %lld годиков", 
+				difference, difference/60, difference/60/60, difference/60/60/24, difference/60/60/24/365);
+
 	return 0;
 }
